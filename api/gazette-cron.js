0e29409a682710b83c8c369b53f554d1e5d7c690
james4ster <james4ster@gmail.com>
2026-02-27 16:52:42 -0500

    debut 2

--- api/gazette-cron.js
+++ api/gazette-cron.js
@@ -1,3 +1,5 @@
+// Removed debugging
+
 export default async function handler(req, res) {
   if (req.headers['authorization'] !== `Bearer ${process.env.CRON_SECRET}`) {
     return res.status(401).json({ error: 'Unauthorized' });

0103d8f99ba4638790d1334816b38c79bc50c91d
Jamie <51055904+james4ster@users.noreply.github.com>
2026-03-21 12:53:36 -0400

    Update avatars-cron.js

--- api/avatars-cron.js
+++ api/avatars-cron.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     const response = await fetch(
-      `${process.env.WN95_SUPABASE_URL}/functions/v1/fetch-avatars`,
+      `${process.env.SUPABASE_URL}/functions/v1/fetch-avatars`,
       {
         method: 'POST',
         headers: {

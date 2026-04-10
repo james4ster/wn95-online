524098aa4d5fbe2766261589e2c7463cad3b19e5
Jamie <51055904+james4ster@users.noreply.github.com>
2026-04-10 15:56:01 -0400

    Add PowerRankings route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ import StreamOverlayMatchupDefault from './overlays/StreamOverlayMatchupDefault'
 import StreamOverlayMatchupIcey from './overlays/StreamOverlayMatchupIcey';
 import PodcastOverlay from './overlays/PodcastOverlay';
 import PodcastCtrl from './overlays/PodcastCtrl';
+import PowerRankings from './pages/PowerRankings';
 //import GettingStarted from './pages/GettingStarted';
 
 const OVERLAY_PATHS = [
@@ -57,6 +58,7 @@ function AppShell() {
         <Route path="/league/:leagueCode/stats" element={<Stats />} />
         <Route path="/league/:leagueCode/managers" element={<Managers />} />
         <Route path="/league/:leagueCode/schedule" element={<Schedule />} />
+        <Route path="/league/:leagueId/power-rankings" element={<PowerRankings />} />
         <Route path="/players" element={<Players />} />
         <Route path="/league/:leagueCode/teams" element={<Teams />} />
         <Route path="/league/:leagueCode/scores" element={<Scores />} />
